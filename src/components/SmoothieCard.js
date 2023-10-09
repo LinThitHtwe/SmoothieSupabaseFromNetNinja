123961466c351904755778f436ed0b48518d9087
LinThit27 <linthit2745@gmail.com>
2023-10-09 10:48:33 +0630

    final version 1.0

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import supabase from "../config/supabaseClient";
-const SmoothieCard = ({ smoothie }) => {
+const SmoothieCard = ({ smoothie, onDelete }) => {
   const handleDelete = async () => {
     const { data, error } = await supabase
       .from("smoothies")
@@ -14,6 +14,7 @@ const SmoothieCard = ({ smoothie }) => {
     }
     if (data) {
       console.log(data);
+      onDelete(smoothie.id);
     }
   };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,20 @@ import SmoothieCard from "../components/SmoothieCard";
 const Home = () => {
   const [error, setError] = useState(null);
   const [smoothies, setSmoothies] = useState(null);
+  const [orderBy, setOrderBy] = useState("created_at");
+
+  const handleDelete = (id) => {
+    setSmoothies((previousSmoothies) => {
+      return previousSmoothies.filter((smoothie) => smoothie.id != id);
+    });
+  };
 
   useEffect(() => {
     const fetchSmoothies = async () => {
-      const { data, error } = await supabase.from("smoothies").select();
+      const { data, error } = await supabase
+        .from("smoothies")
+        .select()
+        .order(orderBy, { ascending: false });
 
       if (error) {
         setError("Cannot fetch");
@@ -21,7 +31,7 @@ const Home = () => {
     };
 
     fetchSmoothies();
-  }, []);
+  }, [orderBy]);
   return (
     <div className="page home">
       <h2>Home</h2>
@@ -29,9 +39,21 @@ const Home = () => {
       {error && <p>{error}</p>}
       {smoothies && (
         <div className="smoothies">
+          <div className="order-by">
+            <p>Order By:</p>
+            <button onClick={() => setOrderBy("created_at")}>
+              Time Created
+            </button>
+            <button onClick={() => setOrderBy("title")}>Title</button>
+            <button onClick={() => setOrderBy("review")}>Rating</button>
+          </div>
           <div className="smoothie-grid">
             {smoothies.map((smoothie) => (
-              <SmoothieCard key={smoothie.key} smoothie={smoothie} />
+              <SmoothieCard
+                key={smoothie.key}
+                smoothie={smoothie}
+                onDelete={handleDelete}
+              />
             ))}
           </div>
         </div>
